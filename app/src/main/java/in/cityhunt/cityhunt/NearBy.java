21ba1ba72d5e5bfb6a5cf6c4250df709e2d486f0
Arunkumar <arunkpkumar.kumar@gmail.com>
2016-01-14 14:56:29 +0530

    some changes

--- app/src/main/java/in/cityhunt/cityhunt/NearBy.java
+++ app/src/main/java/in/cityhunt/cityhunt/NearBy.java
@@ -1,14 +1,14 @@
 package in.cityhunt.cityhunt;
 
 import android.database.Cursor;
-import android.graphics.Camera;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -36,7 +36,7 @@ public class NearBy extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney;
         for (int i=0;i<cursor.getCount();i++){
             sydney = new LatLng(cursor.getDouble(12),cursor.getDouble(13));
-            mMap.addMarker(new MarkerOptions().position(sydney).title(cursor.getString(1)));
+            mMap.addMarker(new MarkerOptions().position(sydney).title(cursor.getString(1)).icon(BitmapDescriptorFactory.fromResource(R.drawable.fests)));
             if (i == cursor.getCount()-1)
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,12));
             cursor.moveToNext();
